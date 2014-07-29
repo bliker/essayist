cfc36065cd4e30f367e005f390f8a4b8d3da15c3
Samuel Vasko <samvasko@gmail.com>
2014-07-29 17:07:26 +0200

    Config files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var gutil = require('gulp-util');
+var glob = require('glob').sync;
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+
+var pkg = require('./package.json');
+var target = './build/';
+
+gulp.task('default', ['build-tests'], function (argument) {
+    gulp.watch(['src/*.js', 'test/*.test.js'], ['build-tests']);
+})
+
+gulp.task('build-tests', function () {
+    var t = browserify({debug: true})
+        .require('./test/undo-manager.test.js', {entry: true});
+
+    t.bundle()
+        .on('error', gutil.log)
+        .pipe(source('tests.js'))
+        .pipe(gulp.dest(target));
+});
+
+gulp.task('clean', function () {
+    gulp.src(target, {read: false}).pipe(clean());
+});
